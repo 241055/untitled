c90e692ea62021c2118d78086e9f2fbeb72b66e3
SakodaTaiga <241055@std.hi-joho.ac.jp>
2025-06-02 12:30:40 +0900

    迫田

---
+++ src/java_text_book/chapter6/calc/CalcLogic.java
@@ -0,0 +1,11 @@
+package java_text_book.chapter6.calc;
+
+public class CalcLogic {
+    public static int tatu(int a, int b) {
+        return (a + b);
+    }
+
+    public static int hiku(int a, int b) {
+        return (a - b);
+    }
+}
